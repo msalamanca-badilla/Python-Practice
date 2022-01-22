c38c83f57fdb31a77e3fd519eb00ba259b005a6e
Marianne <m.salamanca21@live.ca>
2022-01-22 15:09:57 -0500

    exercise 2 complete

--- exercise-2.py
+++ exercise-2.py
@@ -4,13 +4,21 @@
 # 1. Prompts the user to enter a phrase:
 #      Please enter a word or phrase: 
 
-phrase = input('Please enter a word or phrase: ').upper()
+while True:
+
+    phrase = input('Please enter a word or phrase: ').lower()
 
 # 2. Print the following message:
 #      - What you entered is xx characters long
-    print(f'hat you entered is xx characters long')
+    print('What you entered is', len(phrase), 'characters long')
 
 # 3. Return to step 1, unless the word 'quit' was entered.
 
-    if phrase !
 
+    if phrase == 'quit':
+        print(f'I"m leaving!')
+        break
+
+
+
+# python3 exercise-2.py
